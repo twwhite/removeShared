95f36ffe826972796afc48bc3aade56db38e248b
twwhite <twhitefoo@gmail.com>
2015-11-23 23:43:26 -0500

    Update remove.js

--- remove.js
+++ remove.js
@@ -3,3 +3,7 @@
 window.setInterval(function(){
   $("div:contains(' shared ')").closest('[id^=hyperfeed_story_id_]').remove();
 }, 1000);	
+
+// Potential expansion: Add conditional statement that references radio buttons in popup HTML to hide "via" and "liked" posts as well.
+
+
