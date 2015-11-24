5a6e78216bb5ca7080f3863bc66c3e744c7c5f94
Tim White <twhitefoo@gmail.com>
2015-11-24 17:18:54 -0500

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,9 +1,20 @@
 // This javascript function parses through the HTML code on Facebook and matches the string " shared ", then deletes the associated
 // parent node for the uppermost div of the post. It does this every second. If that speed becomes cumbersome, increase it.
 window.setInterval(function(){
-  $("div:contains(' shared ')").closest('[id^=hyperfeed_story_id_]').remove();
-}, 1000);	
-
-// Potential expansion: Add conditional statement that references radio buttons in popup HTML to hide "via" and "liked" posts as well.
-
-
+	chrome.storage.sync.get('shared', function (result) {
+		if(result.shared===true){
+			$("div:contains(' shared ')").closest('[id^=hyperfeed_story_id_]').remove();
+		} 
+	});
+	chrome.storage.sync.get('via', function (result) {
+		if(result.via===true){
+			$("div:contains('via')").closest('[id^=hyperfeed_story_id_]').remove();
+		}
+	});
+	chrome.storage.sync.get('liked', function(result){
+		if(result.liked===true){
+			$("div:contains('liked this')").closest('[id^=hyperfeed_story_id_]').remove();
+		}
+	});
+	}, 1000);	
+	
