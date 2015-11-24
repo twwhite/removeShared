900947313cda1bbf94bf645a56f62b9a1f190941
twwhite <twhitefoo@gmail.com>
2015-11-23 23:26:50 -0500

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,1 +1,5 @@
-window.setInterval(function(){$("div:contains(' shared ')").closest('[id^=hyperfeed_story_id_]').remove();}, 1000);	
+// This javascript function parses through the HTML code on Facebook and matches the string " shared ", then deletes the associated
+// parent node for the uppermost div of the post. It does this every second. If that speed becomes cumbersome, increase it.
+window.setInterval(function(){
+  $("div:contains(' shared ')").closest('[id^=hyperfeed_story_id_]').remove();
+}, 1000);	
